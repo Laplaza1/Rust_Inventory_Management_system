b59530c99cf0dcbf463afee6cd110f6fb7d6505e
Laplaza1 <142614580+Laplaza1@users.noreply.github.com>
2024-08-14 21:33:33 -0500

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,25 @@
+//use std::ffi::CString;
+use std::io;
+//use std::io::empty;
+//use std::io::prelude::*;
+
+fn main() {
+    let mut input = String::new();
+    let x:String = String::from("Lebron James");
+
+    println!("{}","Who is the Greatest basketball player in the world");
+    loop {
+        io::stdin().read_line(&mut input)
+            .expect("Failed to read line");
+        println!("{}", input.trim() == x);
+        //assert_eq!(input.trim(),x);
+        match input.trim().eq(&x.to_string()) {
+            true => println!("Correct!!"),
+            _ => println!("Not Correct")}
+        input.clear();
+        }
+    }
+
+
+
+
